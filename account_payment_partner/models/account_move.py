1627aa2a8bd4de6b0f47e5a1f9871d69214ed3a7
deJong <alban@scalizer.fr>
2025-08-26 13:48:30 +0200

    [18.0][FIX] unit tests

--- account_payment_partner/models/account_move.py
+++ account_payment_partner/models/account_move.py
@@ -140,10 +140,8 @@ class AccountMove(models.Model):
             default_values["payment_mode_id"] = (
                 move.payment_mode_id.refund_payment_mode_id.id
             )
-            # Don't set partner_bank_id for refunds to match standard Odoo behavior
-            # This ensures compatibility with tests expecting partner_bank_id to be False
-            # if move.move_type == "in_invoice":
-            #     default_values["partner_bank_id"] = move.partner_bank_id.id
+            if move.move_type == "in_invoice":
+                default_values["partner_bank_id"] = move.partner_bank_id.id
         return super()._reverse_moves(
             default_values_list=default_values_list, cancel=cancel
         )
